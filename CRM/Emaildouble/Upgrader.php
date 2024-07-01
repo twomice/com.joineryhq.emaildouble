a933a174e5f6f37a0b0453fb345a25444d71684a
Aidan Saunders <aidan.saunders@squiffle.uk>
2024-07-01 13:02:46 +0100

    Civix upgrade

--- CRM/Emaildouble/Upgrader.php
+++ CRM/Emaildouble/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Emaildouble_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Emaildouble_Upgrader extends CRM_Emaildouble_Upgrader_Base {
+class CRM_Emaildouble_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
