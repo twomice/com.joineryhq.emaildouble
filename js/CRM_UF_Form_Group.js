34cd8274a92817908f4e10ca4f7b467b5c049d82
Allen Shaw <allen@JoineryHQ.com>
2026-03-24 19:15:43 -0500

    Make bhfe handling kinder to other extensions.

--- js/CRM_UF_Form_Group.js
+++ js/CRM_UF_Form_Group.js
@@ -1,3 +1,12 @@
 CRM.$(function($) {
+  // Add id attribute to bhfe table, so it's easy to reference later.
+  CRM.$('input#is_emaildouble').closest('table').addClass('emaildouble-bhfe-table');
+
   CRM.$('div.crm-uf-advancesetting-form-block table tbody').append(CRM.$('input#is_emaildouble').closest('tr'));  
+  
+  // Remove the bhfe table, but only if it's empty.
+  if ($('table.emaildouble-bhfe-table tr').length == 0) {
+    $('table.emaildouble-bhfe-table').remove();
+  }
+
 });
